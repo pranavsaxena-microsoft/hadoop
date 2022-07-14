8c72ea403e26f7e8686ca4140ae15c45ecdf8990
Pranav Saxena <pranavsaxena@microsoft.com>
2022-07-14 12:10:19 +0530

    final runs

--- hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/utils/MetricHelper.java
+++ hadoop-tools/hadoop-azure/src/test/java/org/apache/hadoop/fs/azurebfs/utils/MetricHelper.java
@@ -1,5 +1,8 @@
 package org.apache.hadoop.fs.azurebfs.utils;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
@@ -17,28 +20,41 @@ public class MetricHelper {
     }
 
     public static void push(Long latency) {
-        metricUnitQueue.add(new MetricUnit(latency, new Date().toInstant().toEpochMilli()));
+        metricUnitQueue.add(new MetricUnit(new Date().toInstant().toEpochMilli(), latency));
     }
 
     public static void startPlot() {
-        scheduledFuture = new ScheduledThreadPoolExecutor(1).schedule(() -> {
-            List<MetricUnit> filteredMetricUnits;
-            if(metricUnitQueue.size() == 0) {
-                return;
+
+        new Thread(() -> {
+            while(true) {
+                try {
+                    Thread.sleep(ONE_MINUTE_DIFF / 2);
+                    List<MetricUnit> filteredMetricUnits;
+                    if (metricUnitQueue.size() == 0) {
+                        return;
+                    }
+                    while (metricUnitQueue.size() > 0 && metricUnitQueue.peek().time < (new Date().toInstant().toEpochMilli() - ONE_MINUTE_DIFF)) {
+                        metricUnitQueue.remove();
+                    }
+                    filteredMetricUnits = new ArrayList<>(metricUnitQueue);
+                    filteredMetricUnits.sort((m1, m2) -> {
+                        return (int) (m1.latency - m2.latency);
+                    });
+                    if(metricUnitQueue.size() > 0) {
+                        plot(filteredMetricUnits.size(), filteredMetricUnits.get((int) ((filteredMetricUnits.size() - 1) * 0.99)));
+                    }
+                } catch (Exception e) {
+                    int a;
+                    a=1;
+                }
             }
-            while(metricUnitQueue.poll().latency >= (new Date().toInstant().toEpochMilli() - ONE_MINUTE_DIFF)) {
-                metricUnitQueue.remove();
-            }
-            filteredMetricUnits = new ArrayList<>(metricUnitQueue);
-            filteredMetricUnits.sort((m1, m2) -> {
-                return (int) (m1.latency - m2.latency);
-            });
-            plot(filteredMetricUnits.get((int)((filteredMetricUnits.size() - 1)*0.99)));
-        }, ONE_MINUTE_DIFF/2, TimeUnit.MILLISECONDS);
+        }).start();
     }
 
-    private static void plot(MetricUnit metricUnit) {
-
+    private static void plot(int sampleSize, MetricUnit metricUnit) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("/home/pranav/Desktop/metrics.csv", true));
+        bufferedWriter.append(sampleSize + "," + metricUnit.latency + "\n");
+        bufferedWriter.close();
     }
 
     static class MetricUnit {
