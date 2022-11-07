c295fb241de7c160a1cba5b955f29f2f30376520
Steve Loughran <stevel@cloudera.com>
2022-11-07 17:49:04 +0000

    HADOOP-18521. prune map of buffer to reader as the array does it
    Change-Id: If30960d0cf8a501b917f25963bf83f0d1b2d6a8b

--- hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/ReadBufferManager.java
+++ hadoop-tools/hadoop-azure/src/main/java/org/apache/hadoop/fs/azurebfs/services/ReadBufferManager.java
@@ -27,10 +27,8 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
@@ -63,11 +61,6 @@ final class ReadBufferManager {
   private ReadBuffer[] bufferOwners;
 
   private Stack<Integer> freeList = new Stack<>();   // indices in buffers[] array that are available
-
-  /**
-   * Map to track reads in progress.
-   */
-  private final Map<Integer, ReadBuffer> activeReads = new ConcurrentHashMap<>(NUM_BUFFERS);
 
   private Queue<ReadBuffer> readAheadQueue = new LinkedList<>(); // queue of requests that are not picked up by any worker thread yet
   private LinkedList<ReadBuffer> inProgressList = new LinkedList<>(); // requests being processed by worker threads
@@ -681,14 +674,6 @@ final class ReadBufferManager {
   @VisibleForTesting
   void callTryEvict() {
     tryEvict();
-  }
-
-  /**
-   * Get the map of active reads.
-   * @return map of active reads.
-   */
-  Map<Integer, ReadBuffer> getActiveReads() {
-    return activeReads;
   }
 
   /**
